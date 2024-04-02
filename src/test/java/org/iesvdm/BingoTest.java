e865e723b1492f28917aacf3a2f9e7b47d91fb4e
alejadrovidalmata <avidmat0703@g.educaand.es>
2024-04-02 14:25:55 +0200

    Cm

--- src/test/java/org/iesvdm/BingoTest.java
+++ src/test/java/org/iesvdm/BingoTest.java
@@ -1,4 +1,9 @@
 package org.iesvdm;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 public class BingoTest {
 }
--- src/test/java/org/iesvdm/UtilesArraysTest.java
+++ src/test/java/org/iesvdm/UtilesArraysTest.java
@@ -1,4 +1,22 @@
 package org.iesvdm;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class UtilesArraysTest {
+
+    @Test
+    void limpiar() {
+        assertThat(UtilesArrays.limpiar()).isEmpty();
+    }
+
+    @Test
+    void rellenar() {
+        int[] array = new int[10];
+        int elementos = 5;
+
+    }
 }
