e5a9f7cd8d0f0ddc20ea21d0846b7638af609933
Alex <avidmat0703@g.educaand.es>
2024-04-02 17:51:07 +0200

    Commit

--- src/test/java/org/iesvdm/BingoTest.java
+++ src/test/java/org/iesvdm/BingoTest.java
@@ -5,5 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 public class BingoTest {
 }
--- src/test/java/org/iesvdm/UtilesArraysTest.java
+++ src/test/java/org/iesvdm/UtilesArraysTest.java
@@ -7,16 +7,4 @@ import java.util.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class UtilesArraysTest {
-
-    @Test
-    void limpiar() {
-        assertThat(UtilesArrays.limpiar()).isEmpty();
-    }
-
-    @Test
-    void rellenar() {
-        int[] array = new int[10];
-        int elementos = 5;
-    assertThat();
-    }
 }
